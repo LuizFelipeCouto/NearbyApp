7083f94272fa123ca3d4b511ceba6370bcc599b6
lfelipe.dev <129457138+LuizFelipeCouto@users.noreply.github.com>
2024-12-15 15:25:58 -0300

    finalizando o App do curso, usando API, conceitos de MVVM-C, na linguagem swift e UIKit

---
+++ NearbyApp/Sources/Features/Models/PlaceModel.swift
@@ -0,0 +1,21 @@
+//
+//  PlaceModel.swift
+//  NearbyApp
+//
+//  Created by Luiz Felipe Aparecido do Couto on 15/12/24.
+//
+
+import Foundation
+
+struct Place: Decodable {
+    let id: String
+    let name: String
+    let description: String
+    let coupons: Int
+    let latitude: Double
+    let longitude: Double
+    let address: String
+    let phone: String
+    let cover: String
+    let categoryId: String
+}
---
+++ NearbyApp/Sources/Features/Scenes/Splash/SplashFlowDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SplashFlowDelegate.swift
+//  NearbyApp
+//
+//  Created by Luiz Felipe Aparecido do Couto on 14/12/24.
+//
+
+import Foundation
+
+public protocol SplashFlowDelegate: AnyObject {
+    func decideNavigationFlow()
+}
